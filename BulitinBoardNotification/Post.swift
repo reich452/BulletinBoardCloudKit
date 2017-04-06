cbc00ca109403a72f8b8117ebbbfae315eaa28d6
reich452 <reich452@regis.edu>
2017-04-06 15:02:41 -0600

    Added storyboard

---
+++ BulitinBoardNotification/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  BulitinBoardNotification
+//
+//  Created by Nick Reichard on 4/6/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    
+    let postText: String
+    let date: Date 
+}
