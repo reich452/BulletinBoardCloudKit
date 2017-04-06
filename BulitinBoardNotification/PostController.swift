614ffb80e2733e95810e8d03db93e04c26b84ac1
reich452 <reich452@regis.edu>
2017-04-06 17:22:06 -0600

    Added postController

---
+++ BulitinBoardNotification/PostController.swift
@@ -0,0 +1,69 @@
+//
+//  PostController.swift
+//  BulitinBoardNotification
+//
+//  Created by Nick Reichard on 4/6/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+
+class PostCOntroller {
+    
+    static let shared = PostCOntroller()
+    
+    var posts = [Post]() {
+        didSet {
+            // Implement notification for when posts are set
+            DispatchQueue.main.async {
+                let notificationCenter = NotificationCenter.default
+                notificationCenter.post(name: Constants.DidRefreshNotification, object: self)
+            }
+        }
+    }
+    
+    private let cloudKitManager = CloudKitManager()
+    
+    init(){
+        refreshData()
+    }
+    
+    func create(withPost postText: String, andDate date: Date = Date(), completion: @escaping ((Error?) -> Void) = { _ in }) {
+        let post = Post(postText: postText, date: date)
+        cloudKitManager.save(post.cloudKitRecord) { (error) in
+            defer { completion (error) } // instead of calling it at the end over every return
+            if let error = error {
+                NSLog("Error saving \(post.postText) to CloudKit with error:\n\(error)")
+                return
+            }
+            self.posts.insert(post, at: 0)
+        }
+    }
+    
+    func refreshData(completion: @escaping ((Error?) -> Void) = { _ in }) {
+        let sortDescriptors = [NSSortDescriptor(key: Constants.postDateKey, ascending: false)]
+        cloudKitManager.fetchRecords(ofType: Constants.postRecordType, withSortDescriptors: sortDescriptors) { (records, error) in
+            defer { completion(error) }
+            if let error = error {
+                NSLog("Error fetching posts: \n\(error)")
+                return
+            }
+            guard let records = records else { return }
+            self.posts = records.flatMap { Post(cloudKitRecord: $0)}
+        }
+    }
+    
+    func subscribeToPushNotification(completion: @escaping ((Error?) -> Void) = { _ in }) {
+        cloudKitManager.subscribeToCreationOFRecords(withtype: Constants.postRecordType) {
+            (error) in
+            if let error = error {
+                NSLog("Error saving subscription:\n\(error)")
+            } else {
+                NSLog("Subscribed to push notification for new posts")
+            }
+            completion(error)
+        }
+        
+    }
+    
+}
